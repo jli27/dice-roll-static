1cf264bf523f044de7fa57d7041d20ee018485fb
jli27 <96714100+jli27@users.noreply.github.com>
2025-02-07 21:36:12 +0000

    finished

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -6,4 +6,23 @@ class DiceController < ApplicationController
   def two_ten
     render({ :template => "dice_templates/2d10"})
   end
+
+  def homepage
+    render({ :template => "dice_templates/homepage"})
+  end
+
+  def one_twenty
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_four
+    @dice_rolls = Array.new
+
+    5.times do
+      @dice_rolls.push(rand(1..4))
+    end
+
+    render({ :template => "dice_templates/5d4"})
+  end
+
 end
